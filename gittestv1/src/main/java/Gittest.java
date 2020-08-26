9ecb9271613446db9dbfe4108eb91c93d8f945c6
1006623 <zhaoxiaolong@bl.com>
2020-08-26 16:12:33 +0800

    新建文件，打印“111111111111111111”

---
+++ gittestv1/src/main/java/Gittest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 1006623 on 2020/8/26.
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("111111111111111111111111");
+    }
+}
