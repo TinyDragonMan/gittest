458d7721b21bf8042abd253fba2e2525e98aa0d3
1006623 <zhaoxiaolong@bl.com>
2020-08-26 16:47:15 +0800

    主分支新增打印

--- gittestv1/src/main/java/Gittest.java
+++ gittestv1/src/main/java/Gittest.java
@@ -4,5 +4,7 @@
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("111111111111111111111111");
+        System.out.println("master主分支新加了内容");
+
     }
 }
